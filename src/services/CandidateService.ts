2d3735126058bb9761a3aac759e3139b701e1bf2
Efra Peralta <efra23peralta@gmail.com>
2020-08-21 23:23:44 -0400

    feat(candidate): add service

--- src/services/CandidateService.ts
+++ src/services/CandidateService.ts
@@ -1,14 +1,73 @@
 import { config } from '../config'
-import { Candidate } from 'src/database/entities/Candidate'
+import { Candidate } from '../database/entities/Candidate'
 import { CandidateMapper } from '../domain/mappers/CandidateMapper'
 import { CandidateRepository } from '../repositories/CandidateRepository'
 import { CandidateDTO } from '../domain/dtos/CandidateDTO'
 import { ErrorHandler, statusCodes } from '../http'
 import { CandidateMessages } from '../utils/messages/CandidateMessages'
+import { Cloudinary } from '../helpers/Cloudinary'
+import { destroyFiles } from '../helpers/DestroyFiles'
 
 export class CandidateService {
   constructor(
     private _candidateRepository: CandidateRepository,
-    private _candidateMapper: CandidateMapper,
+    private _candidateMapper: CandidateMapper
   ) {}
+
+  mapToEntity = async (payload: any): Promise<Candidate> =>
+    this._candidateMapper.mapToEntity(payload)
+
+  getByEmail = async (email: string) =>
+    this._candidateRepository.getByEmail(email)
+
+  create = async (entity: Candidate): Promise<CandidateDTO> =>
+    this._candidateRepository
+      .save(entity)
+      .then(candidate => this._candidateMapper.mapToDTO(candidate))
+
+  update = async (candidate: Candidate, data: any) =>
+    this._candidateRepository
+      .update(candidate, data)
+      .then(user => this._candidateMapper.mapToDTO(user))
+
+  collection = async (query: { page?: number; perPage?: number }) => {
+    const { page, perPage } = query
+    const options = {
+      page: page || config.PAGINATION.PAGE,
+      perPage: perPage || config.PAGINATION.PER_PAGE,
+    }
+    const { rows, total, pages } = await this._candidateRepository.collection(
+      options
+    )
+
+    if (!rows[0])
+      throw ErrorHandler.build(
+        statusCodes.NOT_FOUND,
+        CandidateMessages.NOT_FOUND
+      )
+
+    return {
+      data: this._candidateMapper.mapListToDTO(rows),
+      meta: {
+        total,
+        pages,
+        nextPage:
+          options.page >= pages ? false : parseInt(options.page as any) + 1,
+      },
+    }
+  }
+
+  async attachment(file: { path: string; name: string }, destroyFile?: string) {
+    if (destroyFile) await Cloudinary.destroy(destroyFile)
+
+    const { secure_url: path, public_id: id } = await Cloudinary.upload(
+      file.path,
+      {
+        folder: 'candidates',
+      }
+    )
+    if (id) await destroyFiles(file.name)
+
+    return { id, path }
+  }
 }
