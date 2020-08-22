941bd82059add2c51964554c88719d18702f2791
Efra Peralta <efra23peralta@gmail.com>
2020-08-21 23:10:57 -0400

    feat: add candidate dto

--- src/domain/dtos/CandidateDTO.ts
+++ src/domain/dtos/CandidateDTO.ts
@@ -3,4 +3,28 @@ import { MapProp } from 'ts-simple-automapper'
 export class CandidateDTO {
   @MapProp()
   id: number
+
+  @MapProp()
+  name: string
+
+  @MapProp()
+  email: string
+
+  @MapProp()
+  attachment: {
+    path: string
+    id: string
+  }
+
+  @MapProp()
+  position: string
+
+  @MapProp()
+  message: string
+
+  @MapProp()
+  subject: string
+
+  @MapProp()
+  queues: []
 }
