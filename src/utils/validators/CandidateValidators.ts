3122ce0ef5d3924b37fc6e9cbbdd6a1fbc9a132d
Efra Peralta <efra23peralta@gmail.com>
2020-08-22 01:18:32 -0400

    feat: add update validator

--- src/utils/validators/CandidateValidators.ts
+++ src/utils/validators/CandidateValidators.ts
@@ -3,13 +3,7 @@ import { CandidateMessages } from '../messages/CandidateMessages'
 
 const { VALIDATOR } = CandidateMessages
 
-const create = [
-  check('name', VALIDATOR.DEFAULT).isLength({
-    min: 2,
-  }),
-  check('email', VALIDATOR.EMAIL)
-    .isEmail()
-    .normalizeEmail({ all_lowercase: true }),
+const common = [
   check('position', VALIDATOR.DEFAULT).optional().isLength({
     min: 2,
   }),
@@ -21,4 +15,25 @@ const create = [
   }),
 ]
 
-export const validators = { create }
+const create = [
+  ...common,
+  check('name', VALIDATOR.DEFAULT).isLength({
+    min: 2,
+  }),
+  check('email', VALIDATOR.EMAIL)
+    .isEmail()
+    .normalizeEmail({ all_lowercase: true }),
+]
+
+const update = [
+  ...common,
+  check('name', VALIDATOR.DEFAULT).optional().isLength({
+    min: 2,
+  }),
+  check('email', VALIDATOR.EMAIL)
+    .optional()
+    .isEmail()
+    .normalizeEmail({ all_lowercase: true }),
+]
+
+export const validators = { create, update }
