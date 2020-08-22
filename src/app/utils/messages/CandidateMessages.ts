66f47f2c12983df65e2f1c6b9de4ab6b784c355f
Efra Peralta <efra23peralta@gmail.com>
2020-08-22 17:48:44 -0400

    feat(candidate): add subscribe messages

--- src/app/utils/messages/CandidateMessages.ts
+++ src/app/utils/messages/CandidateMessages.ts
@@ -7,6 +7,10 @@ export const CandidateMessages = {
   REQUIRE: 'Oops! We have not found the necessary data for this action.',
   NOT_FOUND: 'Oops! We have not found candidates.',
   EMAIL_EXISTS: 'Oops! Email is already registered.',
+  SUBSCRIBE_FAIL:
+    'An error occurred while trying to subscribe to this recruiter.',
+  UNSUBSCRIBE_FAIL:
+    'An error occurred while trying to unsubscribe to this recruiter.',
   INVALID_FILE_EXT:
     'Oops! It does not include an attachment or the extension is incorrect.',
 }
