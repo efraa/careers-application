6166df335c9c4ef2a048594d452c91b5e1502514
Efra Peralta <efra23peralta@gmail.com>
2020-08-21 23:24:05 -0400

    feat(candidate): add controller

--- src/controllers/CandidateController.ts
+++ src/controllers/CandidateController.ts
@@ -4,4 +4,23 @@ import { CandidateMessages } from '../utils/messages/CandidateMessages'
 
 export class CandidateController {
   constructor(private _candidateService: CandidateService) {}
+
+  create = async (payload: any, file: { path: string; name: string }) => {
+    const emailExists = await this._candidateService.getByEmail(payload.email)
+
+    if (emailExists)
+      throw ErrorHandler.build(
+        statusCodes.BAD_REQUEST,
+        CandidateMessages.EMAIL_EXISTS
+      )
+
+    const attachment = await this._candidateService.attachment(file)
+
+    return this._candidateService
+      .mapToEntity({ ...payload, attachment })
+      .then(async candidate => this._candidateService.create(candidate))
+  }
+
+  collection = async (query: { page?: number; perPage?: number }) =>
+    this._candidateService.collection(query)
 }
