3e5d06b75cafb3ccda2f0dbce7fe1e2a190268f7
Efra Peralta <efra23peralta@gmail.com>
2020-08-21 23:09:05 -0400

    feat: add candidate repository

--- src/repositories/CandidateRepository.ts
+++ src/repositories/CandidateRepository.ts
@@ -1,10 +1,32 @@
 import { Repository, getRepository } from 'typeorm'
 import { Candidate } from '../database/entities/Candidate'
-
 export class CandidateRepository {
   private repo: Repository<Candidate>
 
   constructor() {
     this.repo = getRepository(Candidate)
+  }
+
+  create = async (payload: any) => this.repo.create(payload as Candidate)
+
+  save = async (candidate: Candidate) => this.repo.save(candidate)
+
+  getByEmail = async (email: string) => this.repo.findOne({ email })
+
+  update = async (candidate: Candidate, data: any) =>
+    this.save(this.repo.merge(candidate, data))
+
+  collection = async (query: { page: number; perPage: number }) => {
+    const { perPage, page } = query
+    const [rows, count] = await this.repo.findAndCount({
+      skip: perPage * page - perPage,
+      take: perPage,
+    })
+
+    return {
+      rows,
+      total: count,
+      pages: Math.ceil(count / perPage),
+    }
   }
 }
