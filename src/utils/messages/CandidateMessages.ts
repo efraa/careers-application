44f68a54b5364eae48215646c263b711f24c2e9c
Efra Peralta <efra23peralta@gmail.com>
2020-08-22 01:18:48 -0400

    feat: add require message

--- src/utils/messages/CandidateMessages.ts
+++ src/utils/messages/CandidateMessages.ts
@@ -4,6 +4,7 @@ export const CandidateMessages = {
     ATTACHMENT: '',
     DEFAULT: 'Please enter at least 2 or more characters.',
   },
+  REQUIRE: 'Oops! We have not found the necessary data for this action.',
   NOT_FOUND: 'Oops! We have not found candidates.',
   EMAIL_EXISTS: 'Oops! Email is already registered.',
   INVALID_FILE_EXT:
