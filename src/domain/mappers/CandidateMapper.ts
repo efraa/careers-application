f249241356f4377e49082ae966e31019c75399cf
Efra Peralta <efra23peralta@gmail.com>
2020-08-21 23:11:50 -0400

    feat: update candidate mapper

--- src/domain/mappers/CandidateMapper.ts
+++ src/domain/mappers/CandidateMapper.ts
@@ -4,13 +4,16 @@ import { CandidateRepository } from '../../repositories/CandidateRepository'
 import { CandidateDTO } from '../dtos/CandidateDTO'
 
 export class CandidateMapper {
+  private mapper = new Mapper()
+
   constructor(private _candidateRepository: CandidateRepository) {}
 
-  mapToDTO(from: Candidate): CandidateDTO {
-    const candidateDTO: CandidateDTO = new Mapper().map(from, new CandidateDTO())
-    return candidateDTO
-  }
+  mapToDTO = (from: Candidate): CandidateDTO =>
+    this.mapper.map(from, new CandidateDTO())
+
+  mapToEntity = async (from: any): Promise<Candidate> =>
+    this._candidateRepository.create(from)
 
   mapListToDTO = (candidates: Candidate[]): CandidateDTO[] =>
-    candidates.map(candidate => this.mapToDTO(candidate))
+    this.mapper.mapList(candidates, CandidateDTO)
 }
