ab9a6f0f89e9204283e00d9896f1e498a9d0f007
Efra Peralta <efra23peralta@gmail.com>
2020-08-23 02:21:44 -0400

    feat(candidate): update endpoints

--- src/app/routes/Endpoints.ts
+++ src/app/routes/Endpoints.ts
@@ -2,6 +2,7 @@ const candidates = {
   resource: '/candidates',
   collection: '/',
   queues: '/:candidateId/queues',
+  queue: '/:candidateId/queues/:queueId',
   recruiters: '/:candidateId/recruiters',
   document: '/:candidateId',
 }
